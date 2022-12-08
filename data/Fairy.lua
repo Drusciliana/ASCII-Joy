54c2f9f432e494b244e6580a0ae9c72fb9e030e8
Drusciliana <119077493+Drusciliana@users.noreply.github.com>
2022-12-08 18:54:15 -0500

    Add files via upload

---
+++ data/Fairy.lua
@@ -0,0 +1,123 @@
+--[[
+ *
+ * Fairy Message Fun
+ *
+]]--
+    fairy_data = {}
+    fairy_data = {
+        [1] = { -- Idle Catch Messages.
+            [1] =  'Combat in this game is faster than you...';
+            [2] =  'You cannot catch me, silly.';
+            [3] =  'I am way too fast for you, my dear!';
+            [4] =  '*giggles*';
+            [5] =  'Wheeeeeeeeeeeee!';
+            [6] =  'Tryyy Agaaaaain!';
+            [7] =  'I think they have doctors for eyes now...';
+            [8] =  'This is boring...';
+            [9] =  'Annoyed and/or frustrated yet?';
+            [10] = 'I can do this ALLLLL DAAAAY!';
+            [11] = 'You look cute when you are mad...';
+            [12] = 'OHHHHHH! So close!';
+            [13] = 'Just. Give. Up.';
+            [14] = 'If you keep trying, maybe I will let you win... Maybe...';    
+            [15] = 'Whoooooosh!';
+            [16] = 'I bet you could not even catch the Clap!';
+            [17] = 'Hey! Listen!';
+            [18] = 'Hey! Listen!';
+        },
+        [2] = { -- Idle Messages
+            [1] =  'I am SOOOO bored!';
+            [2] =  'Wake up!';
+            [3] =  'Did your mommy tell you to go to bed?';
+            [4] =  '*snores*';
+            [5] =  'Are you seriously going to make a girl wait?';
+            [6] =  'You better not be fishing...';
+            [7] =  'Play the game!';
+            [8] =  '*bakes a cake for Lunk*';
+            [9] =  '*poke*';
+            [10] = 'Laaaa Deee Da De Daaaaaa!';
+            [11] = 'Zilda is not all that pretty... What does he see in her?';
+            [12] = 'Luke. I am your Father...';
+            [13] = 'Red Mages suck. You probably play a Red Mage.';
+            [14] = 'Chocobo are disgusting creatures...';
+            [15] = 'Oh, the Humanity of it all!';
+            [16] = 'I will just fly around here, all lonely...';
+            [17] = '*gouges the eyes out of a Zilda voodoo doll*';
+            [18] = 'What do you get when you put an onion on top of a manure pile? A Tarutaru!';
+            [19] = 'What do you get when you cross a donkey with a dhalmel? An Elvaan!';
+            [20] = 'Hey! Listen!';
+            [21] = 'Hey! Listen!';
+        },
+        [3] = { -- Thief Catch Messages
+            [1] =  'Not me, idiot!';
+            [2] =  'Do I look like a monster?!';
+            [3] =  'Give me a break...';
+            [4] =  'Moron...';
+            [5] =  'This is why we cannot have nice things...';
+            [6] =  'Stop doing that! Fight!';
+            [7] =  'Hey, Jackass!';
+            [8] =  'Stop touching me!';
+            [9] =  'You are so creepy!';
+            [10] = 'Gross..';
+            [11] = 'Focus on the task at hand, please.';
+            [12] = 'Hey! Listen!';
+            [13] = 'Hey! Listen!';
+            [14] = 'Murder-Hobo!';
+            [15] = 'Stabby-Stabby!';
+            [16] = '*giggles insanely with glee*';
+        },
+        [4] = { -- Thief Sneak Attack Messages    
+            [1] =  'Do it!';
+            [2] =  'The Streets will flow with the blood of the Non-believers!';
+            [3] =  'Tough guy, huh? Hitting them in the back?';
+            [4] =  'Yes! Yeeessss! YEEEEESSSSSSS! *cackles*';
+            [5] =  'I want to see it bleed!';
+            [6] =  'What are you waiting for?';
+            [7] =  'Come to the Dark Side.';
+            [8] =  'Its butt is right there! You cannot miss it!';
+            [9] =  'A well-honed blade in the hands of a well-fed idiot.';
+            [10] = 'Fire at will, Captain.';
+            [11] = 'Ewww. I think it ate a Tarutaru.';
+            [12] = 'I am concerned that you like it back here so much...';
+            [13] = 'Kill it! Kill it NOW!';
+            [14] = 'A knife in the back beats two in the bush!';
+            [15] = 'I NEED to see this die!';
+            [16] = 'Kill it with fire!';
+            [17] = 'Hey! Listen!';
+            [18] = 'Hey! Listen!';
+        },
+        [5] = { -- Death Catch Messages
+            [1]  = 'The Dead cannot hurt me!';
+            [2]  = 'Hey! Listen!';
+            [3]  = 'Hey! Listen!';
+            [4]  = 'Should you not be prioritizing life again?';
+            [5]  = 'Cannot catch me in life, cannot catch me in death...';
+            [6]  = 'If you only tried to fight monsters this hard...';
+            [7]  = 'I guess being dead you can do this forever now, too.';
+            [8]  = 'Reattach that missing hand before trying to touch me...';
+            [9]  = 'Why do you even bother?';
+            [10] = 'Think you need a new game, like getting up.';
+            [11] = 'The flies buzzing your corpse are faster than you.';
+            [12] = 'Please. For once. Do something constructive with your time.';
+            [13] = 'You were not good at living, not so good at dying either...';
+        },
+        [6] = { -- Death Messages
+            [1]  = 'I bet that hurt.';
+            [2]  = 'How is the weather down there?';
+            [3]  = 'Gonna get back up?';
+            [4]  = 'Jerk down! Jerk down! Medic!';
+            [5]  = 'No friends to rez you? I can see why.';
+            [6]  = 'I wish you were Zilda right now...';
+            [7]  = 'Hey! Listen!';
+            [8]  = 'Hey! Listen!';
+            [9]  = 'I do not know why I bother trying to always help you...';
+            [10] = 'Think you might be the first case of Rigor-Moron.';
+            [11] = 'Die again. I missed seeing it this time.';
+            [12] = 'If your dying wish was to not be dumb, you did not get it.';
+            [13] = 'Get back in the fight, soldier!';
+            [14] = 'Time keeps on slipping, slipping, slipping...';
+            [15] = 'Please never volunteer to have to protect me...';
+            [16] = '*points at your corpse and laughs maniacally*';
+        }
+    }        
+	return fairy_data;
